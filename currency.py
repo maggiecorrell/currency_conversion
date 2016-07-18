2dfd11fdfd04b3b499cfe6b8e6173fd0f108a666
Maggie Correll <maggie.correll@gmail.com>
2016-07-18 16:47:29 -0400

    added being able to multiply be an int or float

--- currency.py
+++ currency.py
@@ -9,9 +9,17 @@ class Currency:
                self.currency_code == other.currency_code
 
     def __add__(self, other):
-        if self.currency_code == other.currency_code:
+            if self.currency_code != other.currency_code:
+                raise DifferentCurrencyCodeError
             return (self.amount + other.amount, self.currency_code)
 
     def __sub__(self, other):
-        if self.currency_code == other.currency_code:
+            if self.currency_code != other.currency_code:
+                    raise DifferentCurrencyCodeError
             return (self.amount - other.amount, self.currency_code)
+
+    def __mul__(self, enter_num):
+        return (self.amount * float(enter_num), self.currency_code)
+
+class DifferentCurrencyCodeError(Exception):
+    pass
--- test_currency.py
+++ test_currency.py
@@ -1,4 +1,5 @@
-from currency import Currency
+from currency import Currency, DifferentCurrencyCodeError
+from nose.tools import assert_raises
 
 def test_create_currency_with_amount_and_code():
     one_dollar = Currency(1, 'USD')
@@ -35,3 +36,27 @@ def test_subtract_currency_with_same_currency_code():
     curr2 = Currency(44, 'USD')
 
     assert curr1 - curr2 == (92, 'USD')
+
+def test_raise_a_different_currency_code_error_when_adding():
+    curr1 = Currency(50, 'USD')
+    curr2 = Currency(50, 'EUR')
+
+    assert_raises(DifferentCurrencyCodeError, Currency.__add__, curr1, curr2 )
+
+def test_raise_a_different_currency_code_error_when_subtracting():
+    curr1 = Currency(100, 'USD')
+    curr2 = Currency(50, 'EUR')
+
+    assert_raises(DifferentCurrencyCodeError, Currency.__sub__, curr1, curr2 )
+
+def test_multiply_by_int():
+    curr1 = Currency(100, 'USD')
+    enter_number = 9
+
+    assert  curr1 * enter_number == (900, 'USD')
+
+def test_multiply_by_float():
+        curr1 = Currency(100, 'USD')
+        enter_number = 1.97
+
+        assert  curr1 * enter_number == (197, 'USD')
