d9fbbced0d23c41853270d86576ab82a4a5fd348
Maggie Correll <maggie.correll@gmail.com>
2016-07-18 14:45:46 -0400

    Created Currency class

---
+++ currency.py
@@ -0,0 +1,8 @@
+class Currency:
+    
+    def __init__(self, amount, currency_code):
+        self.amount = amount
+        self.currency_code = currency_code
+    #
+    # def __eq__(self, other):
+    #     return self.amount == and self.currency_code == other.currency_code
