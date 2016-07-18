1c53ea52dd04c6d2c023d92bd22a78d4f77193e8
Maggie Correll <maggie.correll@gmail.com>
2016-07-18 14:58:10 -0400

    added test for checking that a currency object does not equal another with a different amount or currency code

--- test_currency.py
+++ test_currency.py
@@ -11,7 +11,15 @@ def test_currencys_can_be_equal():
     curr2 = Currency(99, 'USD')
 
     assert curr1 == curr2
-#
-# def test_currencys_with_different_amounts_are_not_equal():
-#     curr1 = Currency(1, 'USD')
-#     curr2 = Currency(99, 'USD')
+
+def test_currencys_with_different_amounts_are_not_equal():
+    curr1 = Currency(1, 'USD')
+    curr2 = Currency(99, 'USD')
+
+    assert curr1 != curr2
+
+def test_currencys_with_different_currency_codes_are_not_equal():
+    curr1 = Currency(1, 'USD')
+    curr2 = Currency(1, 'EUR')
+
+    assert curr1 != curr2
