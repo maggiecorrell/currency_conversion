453cd8524a2d2e73dc1e977a58705b65f7f5797e
Maggie Correll <maggie.correll@gmail.com>
2016-07-20 21:42:44 -0400

    added functions to convert different currency

--- test_currency_converter.py
+++ test_currency_converter.py
@@ -1,4 +1,34 @@
-from currency_converter import CurrencyConverter
+from currency_converter import CurrencyConverter, UnknownCurrencyCodeError
+from currency import Currency
+from nose.tools import assert_raises
 
-def test_request_currency_code_that_is_equal_to_passed_in():
-    assert currency_converter.convert(Currency(1, 'USD'), 'USD') == Currency(1, 'USD')
+conversion_rates = {'USD': 1.0 , 'EUR': 0.9, 'JPY': 106}
+
+def test_create_currency_with_code_and_rates():
+    converter = CurrencyConverter(conversion_rates)
+
+    assert converter.rates == conversion_rates
+
+def test_currency_code_that_is_equal_to_passed_in():
+    converter = CurrencyConverter(conversion_rates)
+    currency = Currency(1, 'USD')
+    convert_to_code = 'USD'
+    result = converter.convert(currency, convert_to_code)
+
+    assert result == Currency(1, 'USD')
+
+def test_diff_currency_code_that_passed_in():
+    converter = CurrencyConverter(conversion_rates)
+    currency = Currency(1, 'USD')
+    convert_to_code = 'JPY'
+    result = converter.convert(currency, convert_to_code)
+
+    assert result == Currency(106, 'JPY')
+
+def test_raise_error_unknown():
+    converter = CurrencyConverter(conversion_rates)
+    currency = Currency(1, 'USD')
+    convert_to_code = "GBP"
+
+    with assert_raises(UnknownCurrencyCodeError):
+         converter.convert(currency, convert_to_code)
